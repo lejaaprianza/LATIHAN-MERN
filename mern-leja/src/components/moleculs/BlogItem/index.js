5cb4dd27de8d6fed662f94a1619723141d528d58
lejaaprianza <lejaprianza@gmail.com>
2021-02-24 08:05:42 +0700

    penggunaan fungsi useHistory

--- mern-leja/src/components/moleculs/BlogItem/index.js
+++ mern-leja/src/components/moleculs/BlogItem/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { RegisterBg } from '../../../assets'
+import { Button, Gap } from '../../atoms'
 import './blogItem.scss'
 
 const BlogItem = () => {
+    const history = useHistory()
     return (
         <div className="blog-item">
                 <img className="image-thumb" src={RegisterBg} alt="post"/>
@@ -10,6 +13,8 @@ const BlogItem = () => {
             <p className="title">Title Blog</p>
             <p className="author">Author - Date post</p>
             <p className="body">Loerm ipsum</p>
+            <Gap height={20}/>
+            <Button title="View Detail" onClick={()=> history.push('/detail-blog')}/>
         </div>
         </div>
     )
--- mern-leja/src/pages/DetailBlog/index.js
+++ mern-leja/src/pages/DetailBlog/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import { RegisterBg } from '../../assets'
+import { Gap, Link } from '../../components'
 import './detailBlog.scss'
 
 const DetailBlog = () => {
+    const history = useHistory()
     return (
         <div className="detail-blog-wrapper">
             <img className="img-cover" src={RegisterBg} alt="thumb"/>
             <p className="blog-title">Title Blog</p>
             <p className="blog-author">Author - Date Post</p>
             <p className="blog-body ">Content Blog</p>
+            <Gap height={20}/>
+            <Link title="Kembali ke Blog" onClick={()=>history.push('/')}/>
         </div>
     )
 }
