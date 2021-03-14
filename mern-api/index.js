33effcda01b397ce8207ae81daede4dfd37520b1
lejaaprianza <lejaprianza@gmail.com>
2021-03-15 05:42:20 +0700

    update node js

--- mern-api/index.js
+++ mern-api/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 
 const app = express();
+const router= express.Router();
 
-app.use(()=>{
-    console.log('hello server...')
-    console.log('hello lagi...')
-    console.log('hello tiga...')
+router.use('/router', (req,res,next)=>{
+    console.log('url: ', req.originalUrl);
+    console.log('request: ', req.method);
 })
 
-// 20.19
+app.use('/', router);
+// 21.20 - Membuat dan Memahami Basic Router, Request dan Response di NodeJS menit 3.39
 
 app.listen(4000);
