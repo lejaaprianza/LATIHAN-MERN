f51985db50742e8437b1acb97eab1482acec81cb
lejaaprianza <lejaprianza@gmail.com>
2021-03-18 06:07:27 +0700

    update

---
+++ mern-api/src/controllers/products.js
@@ -0,0 +1,4 @@
+exports.createProduct = (req,res,next)=>{
+    res.json({name: "Leja Aprianza",email: "lejaprianza@gmail.com"})
+    next()
+}
--- mern-api/src/routes/products.js
+++ mern-api/src/routes/products.js
@@ -1,8 +1,21 @@
 
 const express = require('express')
 const router= express.Router();
+const productsController = require('../controllers/products')
 
-router.use('/products', (req,res,next)=>{
+router.post('/product', productsController.createProduct)
+
+// E:\kuliah\javascript\react\prawito-hudoro\MERN Tutorial 22.21 - Restructure Projects NodeJS_ExpressJS 6.05
+
+router.get('/products', (req,res,next)=>{
+    res.json({name: "Leja Aprianza",email: "lejaprianza@gmail.com"})
+    next()
+})
+router.put('/products', (req,res,next)=>{
+    res.json({name: "Leja Aprianza",email: "lejaprianza@gmail.com"})
+    next()
+})
+router.delete('/products', (req,res,next)=>{
     res.json({name: "Leja Aprianza",email: "lejaprianza@gmail.com"})
     next()
 })
