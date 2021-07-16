b689ef0895a7c473fd90eefaaff94959b780022f
lejaaprianza <lejaprianza@gmail.com>
2021-07-16 11:42:12 +0700

    ganti data dummy ke API

--- mern-leja/src/pages/Home/index.js
+++ mern-leja/src/pages/Home/index.js
@@ -1,31 +1,43 @@
-import React from 'react'
-import {BlogItem, Button, Gap} from "../../components";
-import './home.scss'
-import {useHistory} from 'react-router-dom'
+import React, { useEffect } from "react";
+import { BlogItem, Button, Gap } from "../../components";
+import "./home.scss";
+import { useHistory } from "react-router-dom";
+import Axios from "axios";
 const Home = () => {
-    const history = useHistory();
-    return (
-        <div className="home-page-wrapper">
-            
-            <div className="create-wrapper">
-                <Button title="create blog" onClick={()=> history.push('/create-blog')}/>
-            </div>
-            <Gap height={20}/>
-            <div className="content-wrapper">
-            <BlogItem/>
-            <BlogItem/>
-            <BlogItem/>
-            <BlogItem/>
-            </div>
-            <div className="pagination">
-                <Button title="Previous"/>
-                <Gap width={20}/>
-                <Button title="Next"/>
-            </div>
-                <Gap height={20}/>
+  const [dataBlog, setDataBlog] = useState([]);
+  useEffect(() => {
+    Axios.get("http://localhost:4000/v1/blog/posts")
+      .then((result) => {
+        console.log("data API", result.data);
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+      });
+  }, []);
+  const history = useHistory();
+  return (
+    <div className="home-page-wrapper">
+      <div className="create-wrapper">
+        <Button
+          title="create blog"
+          onClick={() => history.push("/create-blog")}
+        />
+      </div>
+      <Gap height={20} />
+      <div className="content-wrapper">
+        <BlogItem />
+        <BlogItem />
+        <BlogItem />
+        <BlogItem />
+      </div>
+      <div className="pagination">
+        <Button title="Previous" />
+        <Gap width={20} />
+        <Button title="Next" />
+      </div>
+      <Gap height={20} />
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Home
+export default Home;
